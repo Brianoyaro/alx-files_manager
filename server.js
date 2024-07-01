e4be9296eecb993d927d5acfb3971cced5070771
Brianoyaro <brianoyaro2000@yahoo.com>
2024-07-01 17:40:02 +0300

    fixed app.use checking parameter

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const routes = require('./routes/index.js');
-app.use('', routes);
+app.use('/', routes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server running on port ${port}`));
